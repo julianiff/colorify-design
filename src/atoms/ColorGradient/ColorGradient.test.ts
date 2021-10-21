2047734fb2aaf3be9a1bf6c7438d99683927bd8b
julianiff <julian.iff@helsana.ch>
2021-10-21 13:46:33 +0200

    chore: readd styling for stack and update test

--- src/atoms/ColorGradient/ColorGradient.test.ts
+++ src/atoms/ColorGradient/ColorGradient.test.ts
@@ -13,7 +13,7 @@ describe('ColorContainer', () => {
 
     const shadowroot = el.shadowRoot!.innerHTML;
     expect(shadowroot).to.equal(
-      '<!---->\n      <div class="gradient-container">\n        <colorify-color-tile hex="#f5df66"></colorify-color-tile>\n        <colorify-color-tile hex="#f5df5c"></colorify-color-tile>\n        <colorify-color-tile hex="#f5df4d"></colorify-color-tile>\n        <colorify-color-tile hex="#f5df3e"></colorify-color-tile>\n        <colorify-color-tile hex="#f5df34"></colorify-color-tile>\n      </div>\n    '
+      '<!----><div class="gradient-container"><colorify-color-tile hex="#f5df66"></colorify-color-tile><colorify-color-tile hex="#f5df5c"></colorify-color-tile><colorify-color-tile hex="#f5df4d"></colorify-color-tile><colorify-color-tile hex="#f5df3e"></colorify-color-tile><colorify-color-tile hex="#f5df34"></colorify-color-tile></div>'
     );
   });
 });
